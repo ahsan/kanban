14fdbb30814d5183be2c37ed3e3d04e82ea4f383
Abdullah Ahsan <abdullah.ahsan@xgrid.co>
2018-06-08 03:44:57 +0500

    Finalized the template
    Signed-off-by: Abdullah Ahsan <abdullah.ahsan@xgrid.co>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  backlogCards = [
+    'one',
+    'this is a long item',
+  ];
+  doingCards = [
+  ];
+  doneCards = [
+  ];
+
+  ngOnInit(): void {
+  }
+
+  moveToDoing(card) {
+    console.log(card)
+  }
+
+  moveToDone(card) {
+    console.log(card)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule, MatCardModule, MatGridListModule, MatListModule, MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    // Material modules
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
